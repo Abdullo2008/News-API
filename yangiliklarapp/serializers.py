63fb8c2f4d8bb281d8ec8f96605cde99c21d67e6
Abdullo2008 <igamberdiyevabdullo754@gmail.com>
2024-08-16 16:33:33 +0500

    Adding validators

--- yangiliklarapp/serializers.py
+++ yangiliklarapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import NewsModel, Category
 
@@ -6,7 +7,7 @@ from .models import NewsModel, Category
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsModel
-        fields = '__all__'
+        fields = ['title', 'body', 'category']
         # fields = ['title', 'body', 'category']
         # extra_kwargs = {
         #     'title': {'required': False},
@@ -14,14 +15,70 @@ class NewsSerializer(serializers.ModelSerializer):
         #     'category': {'required': False}
         # }
 
+    def validate(self, data):
+        title = data.get('title', None)
+
+        if title == title.upper():
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': 'Sarlavhaning hamma harflari katta bulishi mumkin emas.'
+                }
+            )
+        if not title.isalpha():
+            raise ValidationError(
+                                {
+                                    "status": False,
+                                    'message': "Yangilikning sarlavhasi harflardan tashkil topishi kerak!"
+                                }
+                            )
+        if title:
+            if NewsModel.objects.filter(title=title).exists():
+                raise ValidationError(
+                    {
+                        'status': False,
+                        'message': 'Yangilik sarlavhasi mavjud! Iltimos boshqa sarlavha yozing'
+                    }
+                )
+
+
+# class NewsSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=255)
+#     body = serializers.CharField()
+#     category = serializers.ChoiceField(Category)
 
 # class New_Patch_Serializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = NewsModel
 
 
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+    def validate(self, data):
+        name = data.get('name', None)
+
+        if name == name.upper():
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': 'Kategoriya nomi hamma harflari katta bulishi mumkin emas.'
+                }
+            )
+        if not name.isalpha():
+            raise ValidationError(
+                                {
+                                    "status": False,
+                                    'message': "Kategoriya nomi harflardan tashkil topishi kerak!"
+                                }
+                            )
+        if name:
+            if NewsModel.objects.filter(name=name).exists():
+                raise ValidationError(
+                    {
+                        'status': False,
+                        'message': 'Ushbu kategoriya nomi mavjud!'
+                    }
+                )
